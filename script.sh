e2b023ff914c1333ac0a164166b724fc84a6e0aa
DunggVN <65992011+dunggvn@users.noreply.github.com>
2025-12-03 09:01:09 +0700

    trying to build without local_manifests

--- script.sh
+++ script.sh
@@ -1,9 +1,11 @@
 #remove the current manifests
 rm -rf .repo/local_manifests
 
-#Syncing rom and device trees
+#Syncing rom
 repo init -u https://github.com/DerpFest-LOS/android_manifest.git -b 15.2 --git-lfs
-git clone https://github.com/dunggvn/local_manifests --depth 1 -b aosp .repo/local_manifests
+#Syncing device trees
+
+#Syncing common trees
 /opt/crave/resync.sh
 
 #building command
