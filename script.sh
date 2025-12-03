4841c369cebdcecb86f3b54334605d2b7beae2b9
DunggVN <65992011+dunggvn@users.noreply.github.com>
2025-12-03 23:27:45 +0700

    Testing DerpFest a14

--- script.sh
+++ script.sh
@@ -1,25 +1,16 @@
 #remove the current manifests
-rm -rf prebuilts/clang/host/linux-x86 device/lge/flashlmdd vendor/lge/flashlmdd device/lge/sm8150-common vendor/lge/sm8150-common kernel/lge/sm8150 hardware/lge
+rm -rf .repo/local_manifests
 
 #Syncing rom
-repo init -u https://github.com/DerpFest-LOS/android_manifest.git -b 15.2 --git-lfs
+#repo init -u https://github.com/DerpFest-LOS/android_manifest.git -b 15.2 --git-lfs
 
-#Syncing device trees
-git clone https://github.com/lge-qcom-dev/android_device_lge_flashlmdd --depth 1 -b lineage-22.2 device/lge/flashlmdd
-git clone https://github.com/lge-qcom-dev/proprietary_vendor_lge_flashlmdd --depth 1 -b lineage-22.2 vendor/lge/flashlmdd
-
-#Syncing common trees
-git clone https://github.com/lge-qcom-dev/android_device_lge_sm8150-common --depth 1 -b lineage-22.2 device/lge/sm8150-common
-git clone https://github.com/lge-qcom-dev/proprietary_vendor_lge_sm8150-common --depth 1 -b lineage-22.2 vendor/lge/sm8150-common
-
-#Syncing hardware and kernel
-git clone https://github.com/lge-qcom-dev/android_kernel_lge_sm8150 --depth 1 -b lineage-22.2 kernel/lge/sm8150
-git clone https://github.com/lge-qcom-dev/android_hardware_lge --depth 1 -b lineage-22.2 hardware/lge
+#Syncing local manifests
+git clone https://github.com/dunggvn/local_manifests --depth 1 -b lineage-21 .repo/local_manifests
 
 #Crave resync script
 /opt/crave/resync.sh
 
 #building command
 source build/envsetup.sh
-lunch lineage_flashlmdd-bp1a-user
+lunch derp_flashlmdd-user
 mka derp
