803fcf53855fb3817fa418402b327922253c3b86
DunggVN <65992011+dunggvn@users.noreply.github.com>
2025-12-04 00:18:07 +0700

    Building DerpFest A15

--- script.sh
+++ script.sh
@@ -2,10 +2,10 @@
 rm -rf .repo/local_manifests
 
 #Syncing rom
-#repo init -u https://github.com/DerpFest-LOS/android_manifest.git -b 15.2 --git-lfs
+repo init -u https://github.com/DerpFest-LOS/android_manifest.git -b 15.2 --git-lfs
 
 #Syncing local manifests
-git clone https://github.com/dunggvn/local_manifests --depth 1 -b lineage-21 .repo/local_manifests
+git clone https://github.com/dunggvn/local_manifests --depth 1 -b lineage-22.2 .repo/local_manifests
 
 #Crave resync script
 /opt/crave/resync.sh
