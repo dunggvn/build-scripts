84f8f15db1e1cc5999f04ca43c221a76c5418918
DunggVN <65992011+dunggvn@users.noreply.github.com>
2025-12-04 07:07:11 +0700

    Adding echo to improve clarity

--- script.sh
+++ script.sh
@@ -1,16 +1,32 @@
 #remove the current manifests
+echo "=====Removing prebuilts...====="
 rm -rf prebuilts/clang/host/linux-x86 .repo/local_manifests
+echo "=====Removing Done====="
 
 #Syncing rom
+echo "=====Initializing ROM...====="
 repo init -u https://github.com/DerpFest-LOS/android_manifest.git -b 15.2 --git-lfs
+echo "=====Initialize Done====="
 
 #Syncing local manifests
+echo "=====Cloning local manifests...====="
 git clone https://github.com/dunggvn/local_manifests --depth 1 -b lineage-22.2 .repo/local_manifests
+echo "=====Local manifests cloned successfully====="
 
 #Crave resync script
+echo "=====Syncing repo...====="
 /opt/crave/resync.sh
+echo "=====Repo sync done====="
 
 #building command
+echo "=====Setting up build env====="
 source build/envsetup.sh
+echo "=====Build env done====="
+
+echo "=====Lunching...====="
 lunch lineage_flashlmdd-bp1a-user
+echo "=====Lunching Done====="
+
+echo "=====Start building ROM====="
 mka derp
+echo "=====ROM Builded successfully=====*
